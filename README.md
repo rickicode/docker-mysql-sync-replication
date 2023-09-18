5649dd31b2ed5fda896bfa545100d5d1e203bf59
ricki <rickicode@gmail.com>
2023-09-18 13:56:11 +0700

    add support multiple database

--- README.md
+++ README.md
@@ -65,7 +65,22 @@ services:
       - DEST_USER=destination_db_user
 
 ```
+### Example
+```
+BACKUP_TIMES=120
+SRC_HOST=source.netq.me
+SRC_PORT=3306
+SRC_NAME=netqdb,wpdb
+SRC_USER=root
+SRC_PASS=whymelord
+DEST_HOST=dest.netq.me
+DEST_PORT=3306
+DEST_NAME=netqdb,wpdb
+DEST_USER=root
+DEST_PASS=whymelord
+```
 
+### ENV
 The important bits are the environment variables, **all of which are required**.
 
 | Environment Variable | Description |
@@ -73,16 +88,28 @@ The important bits are the environment variables, **all of which are required**.
 | BACKUP_TIMES | Backup time in second |
 | SRC_HOST | Source db hostname |
 | SRC_NAME | Source db name |
-| SRC_PASS | Source db password |   
 | SRC_USER | Source db username |
+| SRC_PASS | Source db password |   
 | DEST_HOST | Destination db hostname |
 | DEST_NAME | Destination db name |
-| DEST_PASS | Destination db password |   
 | DEST_USER | Destination db username |
+| DEST_PASS | Destination db password |   
 
 
 DB NAME will be created if not exist.<br>
 The cointainer will run in the background and will automatically restart if the container is stopped or the Docker daemon is restarted.
+
+
+
+### Deploy using the Koyeb button
+
+The fastest way to deploy the Flask application is to click the **Deploy to Koyeb** button below.
+
+[![Deploy to Koyeb](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?name=MYSQL-REPLICATIONtype=docker&image=ghcr.io/rickicode/docker-mysql-sync-replication:latest)
+
+Clicking on this button brings you to the Koyeb App creation page with everything pre-set to launch this application.
+
+
 
 
 ## Powered by <a name = "powered_by"></a>
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,12 @@ set -e
 # Check required environment variables
 #
 echo -e "Checking required environment variables."
-REQUIRED=( SRC_HOST SRC_USER SRC_PASS SRC_NAME DEST_HOST DEST_USER DEST_PASS DEST_NAME )
+REQUIRED=(SRC_HOST SRC_USER SRC_PASS SRC_NAME DEST_HOST DEST_USER DEST_PASS DEST_NAME)
 
-for i in "${REQUIRED[@]}"
-do
+for i in "${REQUIRED[@]}"; do
   if [ -z "${!i}" ]; then
-      echo -e "Environment variable ${i} is required, exiting..."
-      exit 1
+    echo -e "Environment variable ${i} is required, exiting..."
+    exit 1
   fi
 done
 
@@ -21,11 +20,11 @@ done
 #
 
 if [ -z "$DEST_PORT" ]; then
-  DEST_PORT=3306 
+  DEST_PORT=3306
 fi
 
 if [ -z "$SRC_PORT" ]; then
-  DEST_PORT=3306
+  SRC_PORT=3306
 fi
 
 if [ -z "$BACKUP_TIMES" ]; then
@@ -34,7 +33,7 @@ fi
 
 # Function to check if a database exists on a host
 database_exists() {
-    mysql -h "$1" -P "$2" -u "$3" -p"$4" -e "USE $5;" > /dev/null 2>&1
+  mysql -h "$1" -P "$2" -u "$3" -p"$4" -e "USE $5;" > /dev/null 2>&1
 }
 
 start_time=$(date +%s)  # Waktu awal sinkronisasi
@@ -43,67 +42,72 @@ echo -e "Thank you for using docker-mysql-sync-replication by @rickicode"
 
 while true; do
   echo -e "Starting sync..."
-  
+
   while ! mysql -h "$SRC_HOST" -P "$SRC_PORT" -u "$SRC_USER" -p"$SRC_PASS" -e "SELECT 1;" > /dev/null 2>&1; do
-      echo -e "Source host ${SRC_HOST}:${SRC_PORT} not reachable, trying again in 5 seconds..."
-      sleep 5
+    echo -e "Source host ${SRC_HOST}:${SRC_PORT} not reachable, trying again in 5 seconds..."
+    sleep 5
   done
 
-  echo -e "Exporting source database. ${SRC_NAME}"
-  mysqldump \
-  --user="${SRC_USER}" \
-  --password="${SRC_PASS}" \
-  --host="${SRC_HOST}" \
-  --port="${SRC_PORT}" \
-  --skip-set-charset \
-  "${SRC_NAME}" \
-  > /sql/dump.sql
+  # Export all source databases
+  for db_name in $(echo $SRC_NAME | tr ',' ' '); do
+    echo -e "Exporting source database: ${db_name}"
+    mysqldump \
+      --user="${SRC_USER}" \
+      --password="${SRC_PASS}" \
+      --host="${SRC_HOST}" \
+      --port="${SRC_PORT}" \
+      --skip-set-charset \
+      "${db_name}" \
+      > "/sql/${db_name}_dump.sql"
 
-  sed -i 's/utf8mb4/utf8/g' /sql/dump.sql
-  sed -i 's/utf8_unicode_ci/utf8_general_ci/g' /sql/dump.sql
-  sed -i 's/utf8_unicode_520_ci/utf8_general_ci/g' /sql/dump.sql
-  sed -i 's/utf8_0900_ai_ci/utf8_general_ci/g' /sql/dump.sql
-
+    sed -i 's/utf8mb4/utf8/g' "/sql/${db_name}_dump.sql"
+    sed -i 's/utf8_unicode_ci/utf8_general_ci/g' "/sql/${db_name}_dump.sql"
+    sed -i 's/utf8_unicode_520_ci/utf8_general_ci/g' "/sql/${db_name}_dump.sql"
+    sed -i 's/utf8_0900_ai_ci/utf8_general_ci/g' "/sql/${db_name}_dump.sql"
+  done
 
   while ! mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "SELECT 1;" > /dev/null 2>&1; do
-      echo -e "Destination host ${DEST_HOST}:${DEST_PORT} not reachable, trying again in 5 seconds..."
-      sleep 5
+    echo -e "Destination host ${DEST_HOST}:${DEST_PORT} not reachable, trying again in 5 seconds..."
+    sleep 5
   done
 
-  echo -e "Clearing destination database. ${DEST_NAME}"
-  # Check if the destination database exists
-  if database_exists "$DEST_HOST" "$DEST_PORT" "$DEST_USER" "$DEST_PASS" "$DEST_NAME"; then
-    echo -e "Destination database ${DEST_NAME} exists."
-  else
-    echo -e "Creating destination database ${DEST_NAME}."
-    mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "CREATE DATABASE $DEST_NAME;"
-  fi
+  # Create or clear destination databases and load data
+  for db_name in $(echo $DEST_NAME | tr ',' ' '); do
+    echo -e "Clearing destination database: ${db_name}"
+    # Check if the destination database exists
+    if database_exists "$DEST_HOST" "$DEST_PORT" "$DEST_USER" "$DEST_PASS" "${db_name}"; then
+      echo -e "Destination database ${db_name} exists."
+    else
+      echo -e "Creating destination database ${db_name}."
+      mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "CREATE DATABASE ${db_name};"
+    fi
 
-  mysqldump \
-    --user="${DEST_USER}" \
-    --password="${DEST_PASS}" \
-    --host="${DEST_HOST}" \
-    --port="${DEST_PORT}" \
-    --add-drop-table \
-    --no-data "${DEST_NAME}" | \
-    grep -e ^DROP -e FOREIGN_KEY_CHECKS | \
+    echo -e "Clearing existing data in destination database: ${db_name}"
+    mysqldump \
+      --user="${DEST_USER}" \
+      --password="${DEST_PASS}" \
+      --host="${DEST_HOST}" \
+      --port="${DEST_PORT}" \
+      --add-drop-table \
+      --no-data "${db_name}" | \
+      grep -e ^DROP -e FOREIGN_KEY_CHECKS | \
+      mysql \
+      --user="${DEST_USER}" \
+      --password="${DEST_PASS}" \
+      --host="${DEST_HOST}" \
+      --port="${DEST_PORT}" \
+      "${db_name}"
+
+    echo -e "Loading export into destination database: ${db_name}"
     mysql \
-    --user="${DEST_USER}" \
-    --password="${DEST_PASS}" \
-    --host="${DEST_HOST}" \
-    --port="${DEST_PORT}" \
-    "${DEST_NAME}"
-
-  echo -e "Loading export into destination database. ${DEST_NAME}"
-  mysql \
-    --user="${DEST_USER}" \
-    --password="${DEST_PASS}" \
-    --host="${DEST_HOST}" \
-    --port="${DEST_PORT}" \
-    "${DEST_NAME}" \
-    --default-character-set=utf8mb4 \
-    < /sql/dump.sql
-
+      --user="${DEST_USER}" \
+      --password="${DEST_PASS}" \
+      --host="${DEST_HOST}" \
+      --port="${DEST_PORT}" \
+      "${db_name}" \
+      --default-character-set=utf8mb4 \
+      < "/sql/${db_name}_dump.sql"
+  done
 
   end_time=$(date +%s)  # Waktu saat ini
   elapsed_time=$((end_time - start_time))  # Waktu yang telah berlalu dalam detik
@@ -111,5 +115,5 @@ while true; do
 
   minutes=$((BACKUP_TIMES / 60))
   echo -e "Waiting for ${minutes} minutes..."
-  sleep $BACKUP_TIMES 
+  sleep "$BACKUP_TIMES"
 done
