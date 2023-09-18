d02755f132da3cad70f863362f9851c80670bc8e
ricki <rickicode@gmail.com>
2023-09-18 15:18:45 +0700

    fix update

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,16 +61,13 @@ while true; do
   for db_name in $(echo $DATABASE_NAME | tr ',' ' '); do
     DEST_DB_NAME="REPLIKASI_${db_name}" # Nama database tujuan dengan awalan "REPLIKASI_"
     # Check if the destination database exists
-    if database_exists "$DEST_HOST" "$DEST_PORT" "$DEST_USER" "$DEST_PASS" "${DEST_DB_NAME}"; then
+    if database_exists "$DEST_HOST" "$DEST_PORT" "$DEST_USER" "$DEST_PASS" "${DEST_DB_NAME}_CLONE"; then
       echo -e "Destination database ${DEST_DB_NAME} exists."
       echo -e "Renaming destination database to: ${DEST_DB_NAME}_CLONE"
       mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "CREATE DATABASE ${DEST_DB_NAME}_CLONE;"
       mysqldump --user="${DEST_USER}" --password="${DEST_PASS}" --host="${DEST_HOST}" --port="${DEST_PORT}" --skip-lock-tables --add-drop-database --databases "${DEST_DB_NAME}" >"/sql/${DEST_DB_NAME}_dump.sql"
       mysql --user="${DEST_USER}" --password="${DEST_PASS}" --host="${DEST_HOST}" --port="${DEST_PORT}" <"/sql/${DEST_DB_NAME}_dump.sql"
       mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "DROP DATABASE ${DEST_DB_NAME};"
-    else
-      echo -e "Creating destination database ${DEST_DB_NAME}."
-      mysql -h "$DEST_HOST" -P "$DEST_PORT" -u "$DEST_USER" -p"$DEST_PASS" -e "CREATE DATABASE ${DEST_DB_NAME};"
     fi
 
     echo -e "Exporting source database: ${db_name}"
